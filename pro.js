4e0f7a16220d01b9fd514b482768c377559ace02
Vincenzo Piccinonna <vincentbibo@gmail.com>
2024-02-27 20:07:43 +0100

    esercizio27

---
+++ pro.js
@@ -0,0 +1,56 @@
+let num1= 4;
+let num2= 5;
+let num3= 6;
+let num4= 8;
+let num5= 7;
+
+let somma=num1+num2+num3+num4+num5;
+console.log(somma);
+let media= somma/5;
+console.log(media);
+
+console.log(`La somma tra i numeri equivale a ${somma} e la media equivale ${media}`);
+
+
+
+
+let adesso=2024;
+let annonascita=2000;
+let anni= adesso-annonascita;
+
+console.log(`Vincenzo ha ${anni} anni`);
+
+let differenza=100-anni;
+console.log(differenza)
+
+console.log(`Hai ${anni} anni e ti mancanzo ${differenza} per compierne 100`)
+
+
+// let numero=(parseInt(prompt('Inserisci di numero')))
+// console.log(typeof numero)
+// let numero2=(parseInt(prompt('Inserisci di numero')))
+
+
+
+// let somma2= numero+numero2;
+// let differenza2= numero-numero2;
+// let moltiplicazione= numero*numero2;
+// let divisione= numero/numero2;
+// let potenza= numero**numero2;
+// console.log(`Con i numeri da te scelti,i risultati delle operzioni sono: somma ${somma2},sottrazione ${differenza2},moltiplicazione ${moltiplicazione}, potenza ${potenza}`);
+
+let array_1 = [
+    ['un', 'per', 'incatenarli.'],
+    ['Anello', 'trovarli,'],
+    ['ghermirli', 'e'],
+    ['gondor', 'mark'], ];
+  
+  let array_2 = [
+    [['trovarli,']],
+    ['tu,', 'sciocchi'],
+    ['tu,', 'sciocchi', ['padron', 'Sauron']],
+    ['nel', ['fuggite', 'gandalf']],
+    [['domarli,', 'passare'], 'buio']];
+
+
+  console.log(array_1[0][0], array_1[1][0], array_1[0][1], array_2[4][0][0], array_1[0][0], array_1[1][0], array_1[0][1], array_1[2][0], array_1[2][1], array_2[3][0], array_2[4][1], array_1[0][2]);
